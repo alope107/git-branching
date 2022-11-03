e47e36876790baf4ad9482fe64c0f6b796af786e
jbieniosek <jbieniosek@gmail.com>
2022-11-03 10:53:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #creating a simple function
 def my_function(fname, lname):
-  print(f"My first name is {fname} and my last name is{lname}.")
+  print(f"My first name is {fname} and my last name is {lname}.")
 
 my_function("Jasmine", "Lopez")
 
